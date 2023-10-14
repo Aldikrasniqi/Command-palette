b29a39b02b1880a41b889905dd1e7f036307a394
Aldikrasniqi <aldikrasniqi5@gmail.com>
2023-10-14 21:31:29 +0200

    New changes

--- src/data/data.js
+++ src/data/data.js
@@ -5,64 +5,71 @@ export const data = [
     title: 'GraphQl API',
     team: 'Engineering',
     initials: 'JL',
-    members: '12 '
+    members: '12 ',
+    created_at: 'March 19, 2019',
   },
   {
     id: 2,
     title: 'React Developer',
     team: 'Engineering',
     initials: 'JL',
-    members: '12 '
+    members: '12 ',
+    created_at: 'April 19, 2017',
   },
   {
     id: 3,
     title: 'iOS Developer',
     team: 'Engineering',
     initials: 'JL',
-    members: '12 '
+    members: '12 ',
+    created_at: 'May 19, 2019',
   },
   {
     id: 4,
     title: 'Android Developer',
     team: 'Engineering',
     initials: 'JK',
-    members: '12 '
+    members: '12 ',
+    created_at: 'June 19, 2016',
   },
   {
     id: 5,
     title: 'Product Manager',
     team: 'Product',
     initials: 'JL',
-    members: '12 '
+    members: '12 ',
+    created_at: 'July 19, 2011',
   },
   {
     id: 6,
     title: 'Product Designer',
     team: 'Product',
     initials: 'JL',
-    members: '12 '
+    members: '12 ',
+    created_at: 'August 19, 2022',
   },
   {
     id: 7,
     title: 'UX Designer',
     team: 'Product',
-
     initials: 'JL',
-    members: '12 '
+    members: '12 ',
+    created_at: 'September 19, 2014',
   },
   {
     id: 8,
     title: 'UI Designer',
     team: 'Product',
     initials: 'JL',
-    members: '12 '
+    members: '12 ',
+    created_at: 'October 19, 2020',
   },
   {
     id: 9,
     title: 'UX Designer',
     team: 'Product',
-
     initials: 'JL',
-    members: '12 '
+    members: '12 ',
+    created_at: 'November 19, 2012',
   },
 ];
--- src/pages/ContentProjects.jsx
+++ src/pages/ContentProjects.jsx
@@ -1,23 +1,48 @@
 import React from 'react'
 
 function ContentProjects(data) {
+  const statusColor = (status) => {
+    console.log(status);
+    switch (status) {
+      case 'Android Developer': 
+        return 'bg-green-600 rounded-tl rounded-bl text-white';
+      case 'iOS Developer':
+        return 'bg-blue-600 rounded-tl rounded-bl text-white';
+      case 'Web Developer':
+        return 'bg-yellow-600 rounded-tl rounded-bl text-white';
+      case 'UX Designer':
+        return 'bg-red-600 rounded-tl rounded-bl text-white';
+      case 'UI Designer':
+        return 'bg-purple-600 rounded-tl rounded-bl text-white';
+      case 'Product Manager':
+        return 'bg-pink-600 rounded-tl rounded-bl text-white';
+      case 'React Developer':
+        return 'bg-indigo-600 rounded-tl rounded-bl text-white';
+      case 'GraphQl API':
+        return 'bg-amber-600 rounded-tl rounded-bl text-white';
+      case 'Product Designer':
+        return 'bg-blue-600 rounded-tl rounded-bl text-white';
+      default:
+        return 'rounded-tl rounded-bl text-white';
+    }
+  };
   return (
     <div>
        <table className='w-full'>
         <thead className='text-left bg-gray-100 text-gray-500 font-normal'>
           <tr>
             <th className='font-normal'>Project</th>
-            <th className='font-normal'>Project Description</th>
-            <th className='font-normal'>Project Link</th>
-            <th className='font-normal'>Actions</th>
+            <th className='font-normal'>Project Members</th>
+            <th className='font-normal'>Last Updated</th>
+            <th className='font-normal'></th>
           </tr>
         </thead>
         <tbody className='text-left'>
           {data.data.map((item) => (
             <tr key={item.id}>
-              <td>{item.title}</td>
-              <td>{item.team}</td>
-              <td>{item.initials}</td>
+              <td className='p-3'><span className={`${statusColor(item.title)} w-2 h-2 rounded-full`}></span> </td>
+              <td>{item.members}</td>
+              <td>{item.created_at}</td>
               <td><button className='text-[#6d78d9]'>Edit</button></td>
             </tr>
           ))}
